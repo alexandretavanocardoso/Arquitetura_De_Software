39af01cf93e2857af6eb2647b0c083fe55288c24
Alexandre Tavano <AlexandreTavanoDeveloper@outlook.com>
2021-04-12 20:53:48 -0300

    Dependcy Injaction

---
+++ Fundamentos_Da_Arquitetura/Dependecy_Injection/DemoDI/DemoDI/Controllers/FromServicesController.cs
@@ -0,0 +1,14 @@
+﻿using DemoDI.Cases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoDI.Controllers
+{
+    public class FromServicesController : Controller
+    {
+        // Nao recomendavel injetar por prop -- [FromServices] IClienteServices clienteServices
+        public void Index([FromServices] IClienteServices clienteServices)
+        {
+            clienteServices.AdicionarCliente(new Cliente());
+        }
+    }
+}
---
+++ Fundamentos_Da_Arquitetura/Dependecy_Injection/DemoDI/DemoDI/Controllers/ServiceLocatorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DemoDI.Cases;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DemoDI.Controllers
+{
+    // Não é romendavel service Locator
+    public class ServiceLocatorController : Controller
+    {
+        // interface do serviço padrao de injecao de dependecia
+        private readonly IServiceProvider _serviceProvider;
+
+        public ServiceLocatorController(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void Index()
+        {
+            // Retorna null se não estiver registrado
+            // Ve se a interface esta registrada e obter seus serviços
+            _serviceProvider.GetRequiredService<IClienteServices>().AdicionarCliente(new Cliente());
+        }
+    }
+}
